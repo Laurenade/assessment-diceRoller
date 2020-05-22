9b8519d066ccb26ba1c33e855ec3cf38c8822208
Lauren <laurenade8@yahoo.com>
2020-05-21 23:09:36 -0500

    more javascript added

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,13 +10,13 @@
   //     Use Math.random() to simulate rolling a single six-sided die X number of times, 
   //     where X is the number from the text input box. X is also the number of times 
   //     you will loop
-  // 5- Push the result of each roll onto your dieRolls array
-  // 6- Sum all your dice rolls and display the result 
+  // 5----- Push the result of each roll onto your dieRolls array
+  // 6----- Sum all your dice rolls and display the result 
   //     in the Total area on the page using innerHTML
-  //
 
 const textboxInput = document.querySelector('#number-of-dice-rolls')  
 const rollButton = document.querySelector('#roll-button')
+const totalRolls = document.querySelector('#total-rolls')
 const resultsButton = document.querySelector('#roles-results-button')
 const listOfResults = document.querySelector('#list-of-results')
 
@@ -25,19 +25,43 @@ const dieRolls = []                                                            /
 rollButton.addEventListener("click", function() {                              // (2) //    
     let valueEnteredTextbox = textboxInput.value                               // (3) //
         
-    let currentRoll = 0                                                        // (4) //
-        while (currentRoll <= valueEnteredTextbox) { 
+    let currentRoll = 0                                                        // (4...???) //
+        while (currentRoll <= valueEnteredTextbox) {      ///???? not looping with the textbox input amount///
             let randNum = Math.floor(Math.random() * 6) + 1
-   numberRolled = randNum       
+            numberRolled = randNum       
             currentRoll += 1
+        }               
+        
+    dieRolls.push(numberRolled)                                                // (5 ?) //
+                console.log(dieRolls)           
+                                                                                                                                                               
+            const add = (a, b) =>                                              // (6 ?) //  //https://medium.com/@chrisburgin95/rewriting-javascript-sum-an-array-dbf838996ed0
+                    a + b
+            const sum = dieRolls.reduce(add)
+        
+            totalRolls.innerHTML = sum
+})    
 
+//console.log('good')
 
-
-    }
-    let numberRolled = Number(listOfResults.innerHTML)                         // (5) //
-                listOfResults.innerHTML = numberRolled
-})        
-console.log('good')
+        // no. 4 //
+    // 1----- Add a click event listener to your "Show All Rolls" (resultsButton) button
+    // 2----- Loop over the dieRolls array 
+    // 3- Create a new List Item for each number 
+    // 4- Add that List Item to the innerHTML of the "All Rolls" (listOfResults) element
+        
+        resultsButton.addEventListener("click", function() {                 // (1) //
+                                        //???? let toListNumber = ____
+                let number = 0
+            while (number < dieRolls.length) {                               // (2) //
+                toListNumber = dieRolls[number]                              // (3...???) //
+                                        //????  Error:Cannot access'toListNumber'before initialization
+                                        //   at HTMLButtonElement.<anonymous>(main.js:57)
+                number += 1                                 
+            }
+                let toListNumber = Number(listOfResults.innerHTML)           // (4...???) //
+                     listOfResults.innerHTML = toListNumber
+         })
 
 
 
@@ -54,15 +78,15 @@ console.log('good')
         //loop THAT (value entered into textbox) number of times   (loop the value of the textbox (ex:8) the amount of textbox (8) times)
         //each time 'roll' randomly from 1 to 6  (math.random to give randomly 1 to 6)(6 sides on dice)
         //loop a number of times equal to the number-of-dice (number entered by user)
-
-////???////
-        //will have loop, but not over an array here....   no.4 (on total assesment) 'show all rolls'button should do that...when ready
-                // build array by PUSHing to it----dieRolls
-                       
-                       
- 
-                
-
+/////////////////////////
+//Creating a new element and adding it to an existing element which is already 
+//on the page can be done with strings, concatenation, and innerHTML. 
+//Here is an example of one way you might create a new die element 
+//(presuming you have the numberRolled and allRollsElement variables already 
+//defined elsewhere in your code):
+//        const newDiceString = '<li class="dice">' + numberRolled + "</li>";
+//        allRollsElement.innerHTML += newDiceString;
+////////////////////////
 
 
 //////?????/////
@@ -72,8 +96,3 @@ console.log('good')
     //let randNum = Math.random()
 //          Math.floor((Math.random() * 6) + 1)                                     
     //position += 1 
-
- //   change 6 to value of textbox //
-
-
-// let total = __ + ___   (maybe ?)
